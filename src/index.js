455eae77a660179e29a5b8d976e8d05565eab9f9
miracle-alt <8ryabchenko.olga@gmail.com>
2021-09-20 11:12:22 +0300

    parcel

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import validatePassword from './js/validate-password';
+console.log(validatePassword('qweqwe'));
+
+// import qwe from './js/validate-password';
+// import apiService from './js/api-service';
+// import {
+//     fetchUserById, 
+//     updateUserById,
+//     x as name
+// } from './js/api-service';
+// ili tak забрать все срвзу вместе и дефолтное и именное
+import * as apiService from './js/api-service';
+
+// console.log(qwe);
+// console.log(apiService);
+// console.log(updateUserById);
+// console.log(name);
+console.log(apiService);
+
+
---
+++ src/js/api-service.js
@@ -0,0 +1,17 @@
+export default {
+    fetchAllUsers
+}
+
+const fetchAllUsers = () => {
+    console.log('fetchAllUsers');
+}
+
+export const fetchUserById = id => {
+    console.log('fetchUserById');
+}
+
+export const updateUserById = id => {
+    console.log('updateUserById');
+}
+
+export const x = 'Hello Olga Sabadash!';
