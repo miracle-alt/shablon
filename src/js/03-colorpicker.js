ce175aadb0b3f28477f1bf629a378d8101b2a9d5
miracle-alt <8ryabchenko.olga@gmail.com>
2021-09-24 19:40:00 +0300

    localstorage

---
+++ src/js/03-colorpicker.js
@@ -0,0 +1,58 @@
+const paletteContainer = document.querySelector('.js-palette');
+const cardsMarkup = createColorCardsMarkup(colors);
+paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createColorCardsMarkup (colors) {
+    return colors
+    .map(({hex, rgb}) => {
+        return `
+        <div class="color-card">
+                <div 
+                    class="color-swatch" 
+                    data-hex="${hex}" 
+                    data-rgb="${rgb}" 
+                    style="background-color: ${hex};"
+                ></div>
+                <div class="color-meta">
+                    <p>HEX: ${hex}</p>
+                    <p>RGB: ${rgb}</p>
+                </div>
+            </div>
+        `;
+    }).join('');
+    // console.log(markup);
+}
+//---------------------------------------------------------------------
+function onPaletteContainerClick (evt) {
+    const isColorSwatchEl = evt.target.classList.contains('color-swatch');
+
+    if (!isColorSwatchEl) {                               // чтоб клик был только на цвете
+        return;
+    }
+
+    const swatchEl = evt.target;
+    const parentColorCard = swatchEl.closest('.color-card');
+
+    removeActiveCardClass();
+    addActiveCardClass(parentColorCard);
+    setBodyBgColor(swatchEl.dataset.hex);
+}
+
+
+function setBodyBgColor (color) {          // задается цвет боди
+    document.body.style.backgroundColor = color;
+}
+
+function removeActiveCardClass () {             // снимается активная при нажатии на другом цвете
+    const currentActiveCard = document.querySelector('.color-card.is-active');
+
+    if (currentActiveCard) {
+        currentActiveCard.classList.remove('is-active');
+    }
+}
+
+function addActiveCardClass (card) {         // добавить актив на цвет
+    card.classList.add('is-active');
+}  
+
+paletteContainer.addEventListener('click', onPaletteContainerClick);
